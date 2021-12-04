b6ffbabda1c662e7e6b8a6dd0617d136480b3a66
sean <seanpetermcelroy@gmail.com>
2021-12-04 17:22:18 +0000

    added npm to project

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 
-http.createServer(function (req, res) {
+http.createServer((req, res)=> {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello Brosif');
 }).listen(8080);
