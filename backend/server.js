eb47d7c4f16e2b5b270444d526ef90d1b0719a10
sean <seanpetermcelroy@gmail.com>
2021-12-05 19:02:00 +0000

    enabled CORS + JSON response

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,14 @@
-var http = require('http');
+const http = require('http');
+// const PORT = process.env.PORT || 5000
+
 
 http.createServer((req, res)=> {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello Brosif');
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'POST, GET'
+    }
+    res.writeHead(200, headers);
+    res.end(JSON.stringify({message:'Welcome'}));
 }).listen(8080);
+
+// console.log(`Server running on ${PORT}`)
