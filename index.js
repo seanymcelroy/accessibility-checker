621847179abd117252531b350e1814166636b668
Sean MceLroy <seanpetermcelroy@gmail.com>
2021-12-20 02:19:14 +0000

    Search history text stored in set. clear text box

--- index.js
+++ index.js
@@ -1,27 +1,31 @@
 
-const history=[1,2,3]
+const history=new Set();
 const search_box=document.getElementById("search_box")
 const button=document.getElementById("btn")
 button.setAttribute("disabled", true)
 
 search_box.addEventListener("keyup", ()=>{
     const search_box_text=search_box.value;
-    // console.log(search_box_text);
     if(isValidDomain(search_box_text)){
-        button.removeAttribute("disabled")
+        button.removeAttribute("disabled");
     }else{
-        button.setAttribute("disabled", true)
+        button.setAttribute("disabled", true);
         
     }
 })
 
 button.addEventListener("click", ()=>{
     const search_box_text=search_box.value;
-    addSearchHistory(search_box_text)
+    addSearchHistory(search_box_text);
 })
 
 function addSearchHistory(domain){
-    history.push(domain)
+    
+    search_box.value="";
+    if (history.has(domain)){
+        return
+    }
+    history.add(domain);
 
     let listItemNode = document.createElement("LI");
     listItemNode.setAttribute("id", domain);
@@ -35,9 +39,9 @@ function addSearchHistory(domain){
 
 function isValidDomain(search_box_text){
     if (search_box_text.length===0){
-        return false
+        return false;
     }
-    return true
+    return true;
 }
 
 function httpReq(){
@@ -45,13 +49,13 @@ function httpReq(){
     .then(response => {
         // indicates whether the response is successful (status code 200-299) or not
         if (!response.ok) {
-            throw new Error(`Request failed with status ${reponse.status}`)
+            throw new Error(`Request failed with status ${reponse.status}`);
         }
         // Turn text response into json
-        return response.json()
+        return response.json();
     })
     .then(data => {
-        console.log(data)
+        console.log(data);
     })
     .catch(error => console.log(error))
 }
